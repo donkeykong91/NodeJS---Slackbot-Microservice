f08b1fb2e9dd9f35954af794cb0897580bcaeae1
donkeykong91 <darylnator@gmail.com>
2018-07-19 15:27:11 -0700

    Added a console log to let the user know what port and mode (dev or prod) the user is in

--- bin/run.js
+++ bin/run.js
@@ -9,3 +9,9 @@ const http = require("http");
 const server = http.createServer(service);
 
 server.listen(3000);
+
+server.on("listening", function() {
+
+    console.log(`IRIS is listening on ${server.address().port} in ${service.get("env")} mode.`);
+
+});
