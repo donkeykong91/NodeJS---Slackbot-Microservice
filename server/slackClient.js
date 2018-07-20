fbf951ceebbbd78097f0d8425838ef5a54ec4d2b
donkeykong91 <darylnator@gmail.com>
2018-07-19 20:47:44 -0700

    Added event listener if client is authenticated and if it is then hanlder will output name and team name

--- server/slackClient.js
+++ server/slackClient.js
@@ -4,9 +4,25 @@
 const { RTMClient } = require('@slack/client');   
 
 
+function handleOnAuthenticated(rtmStartData) {
+
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+
+}
+
+
+function addAuthenticatedHandler(rtm, handler) {
+
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+
+}
+
+
 module.exports.init = function slackClient(token, logLevel) {
 
     const rtm = new RTMClient(token, {logLevel: "debug"});
+
+    addAuthenticatedHandler(rtm, handleOnAuthenticated);
 
     return rtm;
 
