b5931d6d444c023e5b7c80a528674f72d377b6e1
donkeykong91 <darylnator@gmail.com>
2018-07-20 22:56:34 -0700

    Added wit module that returns a function that within it returns an object with the property ask

---
+++ server/witClient.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+module.exports = function witClient (token) {
+
+    const ask = function ask(message) {
+
+        console.log("ask: " + message);
+
+        console.log("token: " + token);
+
+    }
+
+    return {
+
+        ask: ask
+
+    }
+
+}
