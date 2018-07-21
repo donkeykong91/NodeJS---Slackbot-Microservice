61959bbee0ae0ed6debc2a9def19ae9aa64e11ae
donkeykong91 <darylnator@gmail.com>
2018-07-20 20:38:36 -0700

    Moved rtm object to top and set as module global and sending message back

--- server/slackClient.js
+++ server/slackClient.js
@@ -3,39 +3,43 @@
 
 const RtmClient = require('@slack/client').RTMClient;   
 
+{let rtm = null;
 
-function handleOnAuthenticated(rtmStartData) {
 
-    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+    function handleOnAuthenticated(rtmStartData) {
 
+        console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+
+    }
+
+
+    function handleOnMessage(message) {
+        
+        console.log(message);
+
+        rtm.sendMessage("this is a test message", message.channel, )
+
+    }
+
+
+    function addAuthenticatedHandler(rtm, handler) {
+
+        rtm.on("authenticated", handler);
+
+    }
+
+
+    module.exports.init = function slackClient(token, logLevel) {
+
+        rtm = new RtmClient(token, {logLevel: logLevel});
+
+        addAuthenticatedHandler(rtm, handleOnAuthenticated);
+
+        rtm.on("message", handleOnMessage);
+
+        return rtm;
+
+    }
 }
-
-
-function handleOnMessage(message) {
-    
-    console.log(message);
-
-}
-
-
-function addAuthenticatedHandler(rtm, handler) {
-
-    rtm.on("authenticated", handler);
-
-}
-
-
-module.exports.init = function slackClient(token, logLevel) {
-
-    const rtm = new RtmClient(token, {logLevel: logLevel});
-
-    addAuthenticatedHandler(rtm, handleOnAuthenticated);
-
-    rtm.on("message", handleOnMessage);
-
-    return rtm;
-
-}
-
 
 module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
