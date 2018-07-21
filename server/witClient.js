29c32d978d8c7bb77a385300445e684063dd1b5e
donkeykong91 <darylnator@gmail.com>
2018-07-21 01:27:47 -0700

    Required superagent module, added handleWitResponse that logs response of wit

--- server/witClient.js
+++ server/witClient.js
@@ -1,9 +1,50 @@
 'use strict'
 
 
+const request = require("superagent");
+
+
+function handleWitResponse (res) {
+
+    console.log(res);
+
+}
+
+
 module.exports = function witClient (token) {
 
-    const ask = function ask(message) {
+    const ask = function ask(message, callback) {
+
+
+        request.get("https://api.wit.ai/message")
+
+            .set("Authorization", "Bearer " + token)
+
+            .query({v: "20180720"})
+
+            .query({q: message})
+
+            .end(function (err, res) {
+
+
+                if (err) {
+
+                    return callback(err);
+
+                }
+
+
+                if (res.statusCode != 200) {
+
+                    return callback("Expected status 200 but got " + res.statusCode);
+
+                }
+
+
+                const witResponse = handleWitResponse(res.body);
+
+            });
+
 
         console.log("ask: " + message);
 
