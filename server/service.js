7a36dbbc76124c83e83885222eb4146754e16a05
donkeykong91 <darylnator@gmail.com>
2018-07-25 02:20:30 -0700

    Grabbing intent, port, and ip from where the api is being served

--- server/service.js
+++ server/service.js
@@ -1,9 +1,67 @@
 "use strict"
 
 
-const express = require("express");
+{const express = require("express");
 
-const service = express();
+ const service = express();
 
 
-module.exports = service;
+    {let request = null;
+
+     let respond = null;
+
+     let next = null;
+
+
+        service.get("/service/:intent/:port", async function (expressRequest, expressRespond, expressNext) {
+
+            request = expressRequest;
+
+            respond = expressRespond;
+
+            next = expressNext;
+
+            
+            try {
+                
+                await GetServiceIntentAndIp();
+                
+            } catch (error) {
+
+                console.log(error);
+
+            }
+
+        });
+
+        
+        function GetServiceIntentAndIp () {
+
+
+            var serviceIntent = request.params.intent;
+
+            var servicePort = request.params.port;
+
+
+            var serviceIp = request.connection.remoteAddress.includes("::") ? 
+
+                                    `[${request.connection.remoteAddress}]` : 
+
+                                        request.connection.remoteAddress;
+
+
+            return new Promise( function executor (resolve, reject) {
+
+                resolve(respond.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`}));
+
+                reject(new Error("Could not get either intent, ip, or port number"));
+
+            });
+            
+        }
+
+    }
+
+    module.exports = service;
+
+}
