cc3f3d15078cbd5f145dca64d5a35795860d7f98
donkeykong91 <darylnator@gmail.com>
2018-07-26 22:31:45 -0700

    Added registry to get service and use its properties for the superagentReqeust.get request

--- server/intents/timeIntent.js
+++ server/intents/timeIntent.js
@@ -4,52 +4,61 @@
 const superagentRequest = require("superagent");
 
 
-module.exports.process = async function process(intentData, callback) {
+module.exports.process = async function process(intentData, registry, callback) {
 
 
-    if (intentData.intent[0].value !== "time") {
-
-        return callback(new Error(`Expected time intent, got ${intentData.intent[0].value}`));
-
-    }
+    {const service = registry.get("time");
 
 
-    if (!intentData.location) {
+        if (intentData.intent[0].value !== "time") {
 
-        return callback(new Error("Missing location in time intent"));
+            return callback(new Error(`Expected time intent, got ${intentData.intent[0].value}`));
 
-    }
-
-
-    {const location = intentData.location[0].value;
-
-     let locationApiResponse = null
+        }
 
 
-        try {
+        if (!intentData.location) {
+
+            return callback(new Error("Missing location in time intent"));
+
+        }
 
 
-            {let dateAndTime = locationApiResponse.body.result;
+        if (!service) {
+
+            return callback(false, "No service available");
+
+        }
 
 
-                locationApiResponse = await superagentRequest.get(`http://localhost:3010/service/${location}`);
+        {const location = intentData.location[0].value;
 
-                return callback(false, `In ${location}, it is now ${dateAndTime}`);
+         let locationApiResponse = null
+
+
+            try {
+
+
+                {let dateAndTime = locationApiResponse.body.result;
+
+
+                    locationApiResponse = await superagentRequest.get(`http://${service.ip}:${service.port}/service/${location}`);
+
+                    return callback(false, `In ${location}, it is now ${dateAndTime}`);
+
+
+                }
+
+
+            } catch (error) {
+
+
+                console.log(error);
+
+                return callback(false, `I had a problem finding out the time in ${location}`);
 
 
             }
-
-
-        } catch (error) {
-
-
-            console.log(error);
-
-            console.log(locationApiResponse.body);
-
-
-            return callback(false, `I had a problem finding out the time in ${location}`);
-
 
         }
 
